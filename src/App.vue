9fe4a2e90146135926b8eda03964e8c99372445d
Jonathan Baines <jnbns42@gmail.com>
2022-08-10 21:30:50 +0100

    lol

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="./assets/PXL_20220810_202821480.jpg" class="logo vue" alt="" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
+  height: 100vw;
   padding: 1.5em;
   will-change: filter;
 }
